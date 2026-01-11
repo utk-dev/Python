df7447515dfbd6354209fb8e68963c43f45ef24b
utk.dev <utkershpandyaa@gmail.com>
2026-01-12 00:20:54 +0530

    Add files via upload

---
+++ 02_Advance/02_Recursions/01_recursion_concept.py
@@ -0,0 +1,22 @@
+'''
+factorial(0) = 1
+factorial(1) = 1
+factorial(2) = 2 x 1
+factorial(3) = 3 x 2 x 1
+factorial(4) = 4 x 3 x 2 x 1
+factorial(5) = 5 x 4 x 3 x 2 x 1
+.
+.
+factorial(n) = n x (n-1) x .... x 3 x 2 x 1
+or
+factorial(n) = n x factorial(n-1)
+'''
+#a machine to find factorial of any number
+n = int(input("Enter the number: "))
+
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    return n*factorial(n-1)
+
+print("The factorial of the number is: ", factorial(n))
