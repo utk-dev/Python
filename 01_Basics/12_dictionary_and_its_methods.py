2172baeb9fc154aa6c5e5879a6c0d809dd070aff
utk.dev <utkershpandyaa@gmail.com>
2026-01-12 20:18:05 +0530

    Update 12_dictionary_and_its_methods.py

---
+++ 01_Basics/12_dictionary_and_its_methods.py
@@ -0,0 +1,25 @@
+##dictionary is a collection of key-value pairs.Dictonary is mutable.
+
+#for eg:- we have to store marks of some students out of 30 with their names, so we can save it as dictionary
+marks = {"lavanya":24,
+         "Rohit":12,
+         "Rajat":19,
+         "Vaishnavi":30}
+# print(marks)  #will print a dictionary of names(keys) with their marks(values)
+
+# ## METHODS OF DICTIONARY
+
+# # 1. .items()-returns a list of (key,value) tuples
+# print(marks.items())
+
+
+# # 2. .keys()-will print a list of key values only(same applies for values(.values()))
+# print(marks.keys())
+
+
+# 3. .update()-update the dictionary with the supplied key-values pairs
+marks.update({"Rohit":11})
+print(marks)
+
+# 4. .get(key/value)-returns the value of the specified key
+print(marks.get("Rajat"))
