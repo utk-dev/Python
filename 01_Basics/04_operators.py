e1a74daf0cad6888f4f2a1dc9a7e31adccd648e4
utk.dev <utkershpandyaa@gmail.com>
2026-01-12 20:16:14 +0530

    Update 04_operators.py

---
+++ 01_Basics/04_operators.py
@@ -0,0 +1,27 @@
+# 1.Arithematic Operators
+
+"(a+b)" #Addition
+"(a-b)" #Subtraction
+"(a*b)" #Multiplication
+"(a/b)" #Division
+"(a%b)" #Remainder
+
+# 2.Assignment Operators
+
+"(a = 4)" #assigning the value
+"(b += 1)" #adding the given constant in b's previous value
+"(c *= 3)" #multiplying the given constant in c's previous value
+
+# 3.Logical Operators
+
+"&&" #AND
+"//" #OR
+"!" #NOT
+
+#Comparison Operators
+
+">"
+"<"
+">="
+"<="
+"!=" #not equal to
